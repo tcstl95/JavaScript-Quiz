8a90460a2ee66ed9e83ec12c15e598a2b544617c
tcstl95 <tomcarlson171@gmail.com>
2022-10-09 17:53:16 -0500

    Commit for BCS

--- assets/script.js
+++ assets/script.js
@@ -1,93 +1,87 @@
-var timer= document.querySelector(".timer");
+var timer = document.querySelector(".timer");
 var start = document.querySelector("#start");
+var decreaseEl= document.querySelector(".decrease");
 
+var questionsIndex = 0;
 
-
-
-var timeRemain = 70;
 
 
 var questions = [
-{
-   title: "Question1",
-   choices: ["answer1", "answer2", "answer3","answer4"],
-   answer: "answer 2",
-},
-{
+  {
+    title: "Question1",
+    choices: ["answer1", "answer2", "answer3", "answer4"],
+    answer: "answer2",
+  },
+  {
     title: "Question2",
-   choices: ["answer1", "answer2", "answer3","answer4"],
-   answer: "answer 4",
-},
-{
+    choices: ["answer1", "answer2", "answer3", "answer4"],
+    answer: "answer4",
+  },
+  {
     title: "Question3",
-   choices: ["answer1", "answer2", "answer3","answer4"],
-   answer: "answer 3",
-},
-{
+    choices: ["answer1", "answer2", "answer3", "answer4"],
+    answer: "answer3",
+  },
+  {
     title: "Question4",
-    choices: ["answer1", "answer2","answer3","answer4"],
-    answer: "answer 1",
-},
-
-]
-
+    choices: ["answer1", "answer2", "answer3", "answer4"],
+    answer: "answer1",
+  },
+];
 
 function countdown() {
-    
-    var timerInterval= setInterval(function(){
+    var timeRemain = 70;
+  
+    var timerInterval = setInterval(function () {
+  
+    if (timeRemain > 0) {
+      
+      timer.textContent = timeRemain + " Seconds remaining!!";
+      timeRemain--;
+    }else if (timeRemain === 1){
+        timer.textContent = timeRemain + " Second remaining!!";
         timeRemain--;
-        timer.textContent = timeRemain + " Seconds remaining!!"
-
-        if (timeRemain === 0){
-            clearInterval(timerInterval);
-
-        }
-    }, 1000);
-
-    
+    } else{
+        timer.textContent = '';
+        clearInterval(timerInterval);
+    }
+  }, 1000);
 }
-start.addEventListener("click", function(event){
-    event.preventDefault();
-    console.log('time starter')
-    countdown(0);
+start.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("time starter");
+  countdown();
+});
+
+function createQuestions(idx) {
+  var header = document.createElement("h1");
+
+  header.innerHTML = questions[idx].title;
+
+  document.getElementById("question").append(header);
+
+  for (var i = 0; i < questions[idx].choices.length; i++) {
+    var option = document.createElement("button");
+    option.setAttribute("class", "option");
+    option.addEventListener("click", function (event) {
+        event.preventDefault();
+        questionsIndex++;
+        createQuestions(questionsIndex);
+      });
+    option.innerHTML = questions[idx].choices[i];
+    document.getElementById("question").append(option);
+  }
+}
+
+start.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("inside start btn click");
+  createQuestions(questionsIndex);
+});
+
+decreaseEl.addEventListener("click", function(){
+if (answer === false){
+    timeRemain-5;
+}
 })
 
-function createQuestions(idx){
-
-    var header = document.createElement('h1');
-    
-
-    header.innerHTML = questions[idx].title
-    
-    document.getElementById('question').append(header)
-  
-
-    for(var i= 0; i < questions[idx].choices.length; i++ ){
-        var option = document.createElement('p');
-        option.innerHTML = questions[idx].choices[i];
-        document.getElementById('question').append(option)
-    } 
-   
-   
-}
-
-
- 
-
-start.addEventListener("click", function(event){
-    event.preventDefault();
-        console.log('inside start btn click')
-        createQuestions(0);
-
-    
-    })
-
-
-
-
-
-
-
-
-
-    
